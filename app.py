ee1a4a1717500d0a82b7fd53b5a933081926f187
srijithkp <srijithkariyattil@mailbox.org>
2020-09-13 14:02:40 +0200

    http port change to 5000

--- app.py
+++ app.py
@@ -31,4 +31,4 @@ def weather():
     return render_template('weather.html')
 
 if __name__ == '__main__':
-    app.run(debug=True, port=80, host='0.0.0.0')
+    app.run(debug=True, port=5000, host='0.0.0.0')
