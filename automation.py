374e26da64ccb1f87350163f4290951f5caecc81
srijithkp <srijithkariyattil@mailbox.org>
2020-09-27 23:47:41 +0200

    V_3_0: Automation Manual and Average, HTML Design change

---
+++ automation.py
@@ -0,0 +1,55 @@
+import configparser
+
+##### Variables ####
+automationData = {}
+
+try:
+    config = configparser.ConfigParser()
+    config.read('config.ini')
+except configparser.MissingSectionHeaderError:
+    raise WrongIniFormatError()
+
+
+def automation_main():
+    pass
+
+
+def resetAvgDurationValues():
+    config.read('config.ini')
+    for valve in config.options('WATERING_DURATION_AVG'):
+        config.set('WATERING_DURATION_AVG', valve,str(0))
+    with open('config.ini', 'w') as configfile:  # save
+        config.write(configfile)
+
+
+def updtManualDurationValues(manualValues):
+    config.read('config.ini')
+    #print(manualValues)
+    for valve in config.options('WATERING_DURATION_MANUAL'):
+        #print(f'{valve} : {manualValues[valve]}')
+        config.set('WATERING_DURATION_MANUAL', valve, manualValues[valve])
+    with open('config.ini', 'w') as configfile:  # save
+        config.write(configfile)
+    pass
+
+
+def getAutomationData():
+    config.read('config.ini')
+    for valve in config.options('WATERING_DURATION_AVG'):
+        automationData[valve+'avg'] = config.getint('WATERING_DURATION_AVG', valve)
+    for valve in config.options('WATERING_DURATION_MANUAL'):
+        automationData[valve] = config.getint('WATERING_DURATION_MANUAL', valve)
+
+    #print(f'automationData:{automationData}')
+    return automationData
+
+def automation_main():
+    try:
+        pass
+    except KeyboardInterrupt:
+        pass
+        # GPIO.cleanup()
+
+
+if __name__ == "__main__":
+    autowater()
--- watering.py
+++ watering.py
@@ -3,10 +3,13 @@
 import RPi.GPIO as GPIO
 import configparser
 import datetime
+from automation import getAutomationData
+from flask import render_template
 
 ######### Common #########
 GPIO.setmode(GPIO.BOARD)
-AVG_TIME_UPDATE_THRESHOLD = 5 #seconds
+AVG_TIME_UPDATE_THRESHOLD = 15  # seconds
+ON_TIME_MAX_THRESHOLD = 99  # seconds
 
 try:
     config = configparser.ConfigParser()
@@ -21,7 +24,7 @@ valveOnTime = {}
 valveOnDuration = {}
 valveOnCount = {}
 for valve in config.options('VALVE_PIN_MAP'):
-    valvePinMap[valve] = config.getint('VALVE_PIN_MAP',valve)
+    valvePinMap[valve] = config.getint('VALVE_PIN_MAP', valve)
     valveState[valve] = 'OFF'
     valveOnTime[valve] = datetime.datetime.now()
     valveOnDuration[valve] = 0
@@ -32,52 +35,89 @@ for valve in config.options('VALVE_PIN_MAP'):
 def cntrlValve(valve):
     getCurrValveState()
     if valveState[valve] == 'OFF':
-        print('Switching the '+valve+' ON')
-        # Open the Relay
-
-        GPIO.output(valvePinMap[valve], GPIO.LOW)
-        valveState[valve] = 'ON'
-        valveOnTime[valve] = datetime.datetime.now()
-        #print(valveOnTime[valve])
-        #Switch ON The Pump
-        if valveState['pump'] == 'OFF':
-                GPIO.output(valvePinMap['pump'], GPIO.LOW)
-                valveState['pump'] = 'ON'
+        setValveOn(valve)
     elif valveState[valve] == 'ON':
-        print('Switching the '+valve+' OFF')
-        GPIO.output(valvePinMap[valve], GPIO.HIGH)
-        valveState[valve] = 'OFF'
-        #Save average duration for automation
-        saveAverageDuration(valve)
-        #Switch Off the Pump if no relays are Open
-        checkPump(valve)
+        setValveOff(valve)
+
+def setValveOn(valve):
+    print('Switching the ' + valve + ' ON')
+    # Open the Relay
+    GPIO.output(valvePinMap[valve], GPIO.LOW)
+    valveState[valve] = 'ON'
+    valveOnTime[valve] = datetime.datetime.now()
+    # print(valveOnTime[valve])
+    # Switch ON The Pump
+    if valveState['pump'] == 'OFF':
+		GPIO.output(valvePinMap['pump'], GPIO.LOW)
+		valveState['pump'] = 'ON'
+
+def setValveOff(valve):
+    print('Switching the ' + valve + ' OFF')
+    GPIO.output(valvePinMap[valve], GPIO.HIGH)
+    valveState[valve] = 'OFF'
+    valveOnDuration[valve] = datetime.datetime.now() - valveOnTime[valve]
+    # Save average duration for automation
+    saveAverageDuration(valve)
+    # Switch Off the Pump if no relays are Open
+    checkPump(valve)
 
 def getCurrValveState():
     return valveState
 
+
 def saveAverageDuration(valve):
-    valveOnDuration[valve] = datetime.datetime.now() - valveOnTime[valve]
     valveOnCount[valve] +=1
 
-    if valveOnDuration[valve].seconds > AVG_TIME_UPDATE_THRESHOLD:
-        config.read('config.ini')
-        valveOnTimeCurrAv = config.getfloat('WATERING_DURATION_AVG',valve)
-        print(f'valveOnTimeCurrAv:{valveOnTimeCurrAv}')
-        valveOnTimeNewAv = valveOnTimeCurrAv + ((valveOnDuration[valve].seconds - valveOnTimeCurrAv)/valveOnCount[valve])
-        print(f'valveOnTimeCurrAv:{valveOnTimeCurrAv},valveOnTimeNewAv:{round(valveOnTimeNewAv)}')
-        config.set('WATERING_DURATION_AVG',valve, str(round(valveOnTimeNewAv)))
-        with open('config.ini', 'w') as configfile: #save
-            config.write(configfile)
+    if valve != "pump" and valve != 'fountain':
+        if valveOnDuration[valve].seconds > AVG_TIME_UPDATE_THRESHOLD:
+            config.read('config.ini')
+            valveOnTimeCurrAv = config.getint('WATERING_DURATION_AVG', valve)
+            # print(f'valveOnTimeCurrAv:{valveOnTimeCurrAv}')
+            valveOnTimeNewAv = valveOnTimeCurrAv + \
+                ((valveOnDuration[valve].seconds -
+                  valveOnTimeCurrAv) / valveOnCount[valve])
+            # print(f'valveOnTimeCurrAv:{valveOnTimeCurrAv},valveOnTimeNewAv:{round(valveOnTimeNewAv)}')
+            config.set('WATERING_DURATION_AVG', valve,
+                       str(round(valveOnTimeNewAv)))
+            with open('config.ini', 'w') as configfile:  # save
+                config.write(configfile)
+
 
 def checkPump(valve):
-    for valve,state in valveState.items():
+    for valve, state in valveState.items():
         if valve == 'pump':
-            #Finished checking all valves and none are open; Turn off the pump
+            # Finished checking all valves and none are open; Turn off the pump
             GPIO.output(valvePinMap['pump'], GPIO.HIGH)
             valveState['pump'] = 'OFF'
         else:
             if state == 'ON':
                 break
+
+
+def runAutomatedWatering(type):
+    valveConfigDuration = {}
+    config.read('config.ini')
+    if type == 'Average':
+        for valve in config.options('WATERING_DURATION_AVG'):
+            valveConfigDuration[valve] = config.getint('WATERING_DURATION_AVG', valve)
+    elif type == 'Manual':
+        for valve in config.options('WATERING_DURATION_MANUAL'):
+            valveConfigDuration[valve[:-3]] = config.getint('WATERING_DURATION_MANUAL', valve)
+
+    print(valveConfigDuration)
+    for key in valveConfigDuration:
+        valveCurrOnDuration = 0
+        valveOnTime = datetime.datetime.now()
+        setValveOn(key)
+        while (valveCurrOnDuration < valveConfigDuration[key]):
+            valveCurrOnDuration = (datetime.datetime.now() - valveOnTime).seconds
+            #print(f'{key}: {valveCurrOnDuration}')
+        else:
+            setValveOff(key)
+    print(valveConfigDuration)
+
+    # setValveOn()
+    # setValveOff()
 
 def gpio_cleanup():
     pass
@@ -85,7 +125,7 @@ def gpio_cleanup():
 
 def getLastWatered():
     try:
-		pass
+        pass
     except:
         return "NEVER!"
 
